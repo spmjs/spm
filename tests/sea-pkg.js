b6bbed9d02e8faa2441d65010373c8504c6a3078
U-wenhe-e4310\wenhe <wenhe@wenhe-e4310.(none)>
2011-05-26 13:56:18 +0800

    add anotation parser

---
+++ tests/sea-pkg.js
@@ -0,0 +1,11 @@
+/**
+ * @name KISSY
+ * @desc An Enjoyable UI Library
+ * @url http://kissyui.com/
+ *
+ * @tags base framework,dom utility
+ *
+ * @src http://a.tbcdn.cn/s/kissy/1.2.0/seed.js
+ * @min http://a.tbcdn.cn/s/kissy/1.2.0/seed-min.js
+ *
+ */
