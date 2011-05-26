212fb151fad77910e5f0c537fab6046d85dde5e1
yyfrankyy <yyfrankyy@gmail.com>
2011-05-26 23:38:15 +0800

    handle request data from http: protocol

--- test/spm/transport.js
+++ test/spm/transport.js
@@ -5,12 +5,15 @@ var content = fs.readFileSync(__dirname + '/spec.js', 'utf8');
 var o = transport.parse(content);
 if (o.meta.src.length > 0) {
     transport.getData(o.meta.src[0], function(data) {
-        console.log(data);
+        transport.getVersion(data);
+        // console.log(data);
     });
 }
 
+/*
 if (o.meta.min.length > 0) {
     transport.getData(o.meta.min[0], function(data) {
         console.log(data);
     });
 }
+*/
