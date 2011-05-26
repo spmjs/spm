45c211f111e8263841a27c6ec557187660dce534
yyfrankyy <yyfrankyy@gmail.com>
2011-05-26 17:59:55 +0800

    use multiline replace for spliting comment and function

---
+++ test/spm/annotation-spec.js
@@ -0,0 +1,16 @@
+/**
+ * @name KISSY
+ * @author lifesinger
+ * @author yiminghe
+ * @author TaobaoUED f2es
+ * @desc An Enjoyable UI Library
+ * @url http://kissyui.com/
+ *
+ * @tags base framework,dom utility
+ * @tags all in one
+ *
+ * @src http://a.tbcdn.cn/s/kissy/1.2.0/seed.js
+ * @min http://a.tbcdn.cn/s/kissy/1.2.0/seed-min.js
+ */
+define(function() {
+});
--- test/spm/transport.js
+++ test/spm/transport.js
@@ -1,15 +1,5 @@
-/**
- * @name KISSY
- * @author lifesinger
- * @author yiminghe
- * @author TaobaoUED f2es
- * @desc An Enjoyable UI Library
- * @url http://kissyui.com/
- *
- * @tags base framework,dom utility
- * @tags all in one
- *
- * @src http://a.tbcdn.cn/s/kissy/1.2.0/seed.js
- * @min http://a.tbcdn.cn/s/kissy/1.2.0/seed-min.js
- *
- */
+var fs = require('fs'),
+    transport = require('../../lib/transport');
+
+var content = fs.readFileSync(__dirname + '/annotation-spec.js', 'utf8');
+transport.parse(content);
