ba07a35b59508c64c4d09293ff8e45b5617e51d1
yyfrankyy <yyfrankyy@gmail.com>
2011-05-26 14:18:34 +0800

    change some test specs

--- tests/anotation.js
+++ tests/anotation.js
@@ -8,5 +8,6 @@ assert.equal(o.name[0], 'KISSY');
 assert.equal(o.desc[0], 'An Enjoyable UI Library');
 assert.equal(o.url[0], 'http://kissyui.com/');
 assert.equal(o.tags[0], 'base framework,dom utility');
+assert.equal(o.tags[1], 'all in one');
 assert.equal(o.src[0], 'http://a.tbcdn.cn/s/kissy/1.2.0/seed.js');
 assert.equal(o.min[0], 'http://a.tbcdn.cn/s/kissy/1.2.0/seed-min.js');
--- tests/sea-pkg.js
+++ tests/sea-pkg.js
@@ -4,6 +4,7 @@
  * @url http://kissyui.com/
  *
  * @tags base framework,dom utility
+ * @tags all in one
  *
  * @src http://a.tbcdn.cn/s/kissy/1.2.0/seed.js
  * @min http://a.tbcdn.cn/s/kissy/1.2.0/seed-min.js
