3978b49a6ea53d5265b4f2c25517caabbb249f97
lifesinger <lifesinger@gmail.com>
2011-05-30 11:45:22 +0800

    add kissy

---
+++ modules/kissy/transport.js
@@ -0,0 +1,18 @@
+/**
+ * @name KISSY
+ * @desc An Enjoyable UI Library.
+ * @author Taobao UED
+ * @url http://kissyui.com/
+ * @tags dom,event,library,ui library
+ * @version 1.2.0
+ *
+ * @src http://a.tbcdn.cn/s/kissy/1.2.0/kissy.js
+ * @min http://a.tbcdn.cn/s/kissy/1.2.0/kissy-min.js
+ */
+define(function(require, exports, module) {
+
+/*{{code}}*/
+
+  module.exports = KISSY;
+  try { delete this.KISSY; } catch(ex) { this.KISSY = undefined; }
+});
