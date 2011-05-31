91c1c968452c02fcf2df8388e9a8aeaa7b297563
lifesinger <lifesinger@gmail.com>
2011-05-31 10:18:47 +0800

    improve alias parser logic

--- test/sbuild/config-alias/app-config.js
+++ test/sbuild/config-alias/app-config.js
@@ -3,7 +3,7 @@ seajs.config({
       timeout: 5000,
       alias: {
         'increment': 'increment.js?t=20110530',
-        'lib': './sub/lib',
+        'lib': './lib',
         'underscore': 'underscore/1.1.6/underscore'
       },
       debug: true
