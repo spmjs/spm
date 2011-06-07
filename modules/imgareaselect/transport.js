1ef0e6ac2d6c6908d29b75b349a98d6d053653cc
lifesinger <lifesinger@gmail.com>
2011-06-07 21:49:26 +0800

    add modules/imgareaselect

---
+++ modules/imgareaselect/transport.js
@@ -0,0 +1,19 @@
+/**
+ * @name imgareaselect
+ * @desc imgAreaSelect is a jQuery plugin for selecting a rectangular area
+ *       of an image. It allows web developers to easily implement image
+ *       cropping functionality, as well as other user interface features,
+ *       such as photo notes (like those on Flickr).
+ * @author Michal Wojciechowski
+ * @url http://odyniec.net/projects/imgareaselect/
+ * @tags jquery plugin
+ * @version 0.9.6
+ *
+ * @src http://odyniec.net/projects/imgareaselect/jquery.imgareaselect.dev.js
+ * @min http://odyniec.net/projects/imgareaselect/jquery.imgareaselect.min.js
+ */
+define(function() { return function(jQuery) {
+
+/*{{code}}*/
+
+}});
