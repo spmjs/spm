b681bf49f0789b36009c79205ff495138362dcfd
yyfrankyy <yyfrankyy@gmail.com>
2011-06-10 17:21:41 +0800

    add a default path for local package.json default as: transports/

--- test/spm/transport/test.js
+++ test/spm/transport/test.js
@@ -1,7 +1,8 @@
 // vim: set ts=2 sw=2:
 var assert = require('assert'),
     fs = require('fs'), path = require('path'),
-    util = require('../../../lib/util');
+    util = require('../../../lib/util'),
+    console = require('../../../lib/spm/log');
 
 var getFile = function(p) {
   return path.join(__dirname, p);
@@ -9,11 +10,43 @@ var getFile = function(p) {
 
 var transport = require('../../../lib/spm/transport');
 
-exports.run = function() {
-  util.readFromPath(getFile('remote-package.tspt'), function(comments) {
-    transport.parseTemplate(comments, function(o) {
-      var meta = o.meta;
-      assert.equal(meta.name, 'SeaJS');
-    });
-  });
+var justCall = function(fn) {
+  fn();
 };
+
+exports.run = function() {
+  specs.forEach(justCall);
+};
+
+var specs = [
+  function() {
+    util.readFromPath(getFile('remote-package.tspt'), function(comments) {
+      transport.parseTemplate(comments, function(o) {
+        var meta = o.meta;
+        assert.equal(meta.name, 'SeaJS', 'support remote json');
+      });
+    });
+  },
+  function() {
+    util.readFromPath(getFile('remote-package-overwrite.tspt'),
+        function(comments) {
+          transport.parseTemplate(comments, function(o) {
+            var meta = o.meta;
+            assert.equal(
+                meta.version, '0.1.0',
+                'support remote json overwrite');
+          });
+        }
+    );
+  },
+  function() {
+    util.readFromPath(getFile('local-package.tspt'),
+        function(comments) {
+          transport.parseTemplate(comments, function(o) {
+            var meta = o.meta;
+            assert.equal(meta.name, 'SeaJS', 'support local json');
+          });
+        }
+    );
+  }
+];
