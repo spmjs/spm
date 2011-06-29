39e203065d5f27a923b2f118cb76bf6563204acb
yyfrankyy <yyfrankyy@gmail.com>
2011-06-29 11:56:32 +0800

    change modules directory to modules.seajs.com

---
+++ support/deploy_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# @file deploy_modules.sh
+# @fileoverview: deploy modules.seajs.com
+# @author: yyfrankyy(yyfrankyy@gmail.com)
+# @usage: ./deploy_modules.sh 
+# @version: 1.0
+# @create: 06/29/2011 11:33:00 AM CST
+
+cd modules.seajs.com
+vmc update seajs
