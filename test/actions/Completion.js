66990eda590f3071861a8f8b67db417273a354b8
lifesinger <lifesinger@gmail.com>
2011-10-05 16:16:38 +0800

    code refactoring

--- test/actions/Completion.js
+++ test/actions/Completion.js
@@ -7,18 +7,32 @@ require('colors');
 var path = require('path');
 var assert = require('assert');
 
+var CONFIG = require('../../lib/config');
 var Completion = require('../../lib/actions/Completion');
 
 
 var testName = path.basename(__filename);
 console.log(('test ' + testName).cyan);
 
-var comp;
+var comp, expected;
 
 
 // {{{
 console.log('  test spm [TAB]');
+expected = CONFIG.AVAILABLE_ACTIONS.join(' ').replace(' completion', '');
+
 comp = new Completion('spm');
+assert.equal(comp.run(), expected);
+
+comp = new Completion('spm b');
+assert.equal(comp.run(), expected);
+// }}}
+
+
+// {{{
+console.log('  test spm action [TAB]');
+
+comp = new Completion('build');
 console.log(comp.run());
 // }}}
 
