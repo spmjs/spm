d3f876ecbcc6b36032538cab09a6574b4e3ca114
lifesinger <lifesinger@gmail.com>
2011-10-05 22:29:23 +0800

    code refactoring

---
+++ test/actions/Help.js
@@ -0,0 +1,35 @@
+/**
+ * @author lifesinger@gmail.com (Frank Wang)
+ */
+
+require('colors');
+
+var path = require('path');
+var assert = require('assert');
+
+var Help = require('../../lib/actions/Help');
+
+
+var testName = path.basename(__filename);
+console.log(('test ' + testName).cyan);
+
+
+// {{{
+console.log('  test spm help');
+assert.ok(new Help().run().indexOf('SeaJS Package Manager') > -1);
+// }}}
+
+
+// {{{
+console.log('  test spm help build');
+assert.ok(new Help('build').run().indexOf('build') > -1);
+// }}}
+
+
+// {{{
+console.log('  test spm help not-exists');
+assert.ok(new Help('xx').run().indexOf('Unknown') > -1);
+// }}}
+
+
+console.log((testName + ' is ').cyan + 'PASSED'.green);
--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ require('colors');
 require('./actions/Action');
 require('./actions/Build');
 require('./actions/Completion');
+require('./actions/Help');
 
 require('./utils/Annotation');
 require('./utils/Combo');
