b5a280735572f4f271a45b2fd500655b7ed93561
lifesinger <lifesinger@gmail.com>
2011-11-05 19:40:42 +0800

    update install.sh for node v0.6.0

--- install.sh
+++ install.sh
@@ -2,29 +2,29 @@
 #usage: . install.sh
 
 touch ~/.bash_profile
-if [ `grep -c "#add ~/local/bin to PATH" ~/.bash_profile` == 0 ]
-  then
-    echo 'export PATH=$HOME/local/bin:$PATH #add ~/local/bin to PATH' >> ~/.bash_profile
-    . ~/.bash_profile
+if [ `grep -c "#add ~/local/bin to PATH" ~/.bash_profile` == 0 ]; then
+  echo Set PATH...
+  echo 'export PATH=$HOME/local/bin:$PATH #add ~/local/bin to PATH' >> ~/.bash_profile
+  . ~/.bash_profile
 fi
 
-if [ -z `which node` ]
-  then
-    mkdir ~/tmp
-    rm -rf ~/tmp/node-install
-    mkdir ~/tmp/node-install
-    cd ~/tmp/node-install
-    curl http://nodejs.org/dist/v0.5.9/node-v0.5.9.tar.gz | tar xz --strip-components=1
-    mkdir ~/local
-    ./configure --prefix=~/local
-    sudo make
-    sudo make install # ok, fine, this step probably takes more than 30 seconds...
+if [ -z "`which node`" -o -z "`node --version | grep v0.6`" ]; then
+  echo Install node...
+  mkdir ~/tmp
+  rm -rf ~/tmp/node-install
+  mkdir ~/tmp/node-install
+  cd ~/tmp/node-install
+  curl http://nodejs.org/dist/v0.6.0/node-v0.6.0.tar.gz | tar xz --strip-components=1
+  mkdir ~/local
+  ./configure --prefix=~/local
+  sudo make
+  sudo make install # ok, fine, this step probably takes more than 30 seconds...
 fi
 
-if [ -z `which npm` ]
-  then
-    curl http://npmjs.org/install.sh | sudo sh
+if [ -z "`which npm`" -o -z "`npm --version | grep 1.0`" ]; then
+  echo Install npm...
+  curl http://npmjs.org/install.sh | sudo sh
 fi
 
-echo Installing spm...
+echo Install spm...
 sudo npm install -g https://github.com/seajs/spm/tarball/master
