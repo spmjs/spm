249d49e94579a02edb060e3eb88f7a22db0167fc
lifesinger <lifesinger@gmail.com>
2011-12-02 21:16:42 +0800

    bug fix for conditional-compiler

--- test/issues/conditional-compile/build-config.js
+++ test/issues/conditional-compile/build-config.js
@@ -1,5 +1,7 @@
 module.exports = {
 
+  app_url: 'http://test.com/js/',
+
   compiler_options: {
     'defines': {
       'LANG': ['string', 'zh-cn']
--- test/issues/conditional-compile/expected/main.js
+++ test/issues/conditional-compile/expected/main.js
@@ -1,1 +1,1 @@
-define(["./zh-cn.js"],function(a){var b=this.LANG="zh-cn";b=a("./zh-cn.js")});
+define("http://test.com/js/main.js",["./zh-cn.js"],function(a){var b=this.LANG="zh-cn";b=a("./zh-cn.js")});
