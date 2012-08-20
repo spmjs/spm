be19efeb013460a8e0f58d37aeab2f5ce14dc949
kangpangpang <kangpangpang@gmail.com>
2012-08-20 18:11:28 +0800

    fix #235

--- test/data/modules/relativeModule/dist/core/a-debug.js
+++ test/data/modules/relativeModule/dist/core/a-debug.js
@@ -1,11 +1,11 @@
-define("#relativeModule/0.9.1/b-debug", [], function(require, exports){
+define("http://arale.alipay.im:8000/relativeModule/0.9.1/b-debug", [], function(require, exports){
   exports.say = function() {
     console.log('say!'); 
   };
 });
 
 
-define("#relativeModule/0.9.1/core/a-debug", ["./b-debug"], function(require, exports) {
+define("http://arale.alipay.im:8000/relativeModule/0.9.1/core/a-debug", ["./b-debug"], function(require, exports) {
   var b = require('./b-debug');
   exports.say = function() {
     b.say();
