d93a3d1a6b5ab74ffe02175e381f2cdf39ed3a77
kangpangpang <kangpangpang@gmail.com>
2012-08-28 00:17:45 +0800

    add config_parset test and add parent parse #240 #245

--- test/core/config_parse_spec.js
+++ test/core/config_parse_spec.js
@@ -48,21 +48,99 @@ describe("config parse test", function() {
     expect(config._state).toBe('end');
   });
 
-  it('test local config read', function() {
-    var config = new Config();
-    var json = path.join(path.dirname(module.filename), "../data/core/config_parse/config.json");
-    
-    console.info('----1-->', config._state);
-    config.addFile(json);
-    console.info('----2-->', config._state, config.get('name'));
+  var json1 = path.join(path.dirname(module.filename), "../data/configs/config_1.json");
+  var json2 = path.join(path.dirname(module.filename), "../data/configs/config_2.json");
+  var json3 = path.join(path.dirname(module.filename), "../data/configs/config_3.json");
+  var p_config = path.join(path.dirname(module.filename), "../data/configs/parent_config.json");
+  var spm_config = path.join(path.dirname(module.filename), "../data/configs/spm_config.json");
+  var seajs_config = path.join(path.dirname(module.filename), "../data/configs/seajs.config");
+
+  var config;
+  
+  beforeEach(function() {
+    config = new Config();
+    config.addFile(json1);
+  });
+
+  afterEach(function() {
+    config = null; 
+  });
+
+  it('test local and url config read', function() {
+    config.on('end', function(config) {
+      // test parse end
+      expect(config.get('sources').length).toEqual(3);
+      expect(Object.keys(config._obj).length).toBeGreaterThan(0);
+    });
 
     runs(function() {
+      config.addFile(json2);
+      config.addUrl('http://modules.seajs.org/config.json');
     });
 
     waitsFor(function() {
-     
-    }, 1000);
+      return config._state === 'end'; 
+    }, 6000);
 
-    console.info('----->', config._state);
+    runs(function() {
+      expect(config.get('name')).toBe('module');
+      expect(config.get('version')).toBe('0.0.1');
+
+      // test array merge
+      var sources = config.get('sources');
+      expect(sources.length).toBe(3);
+      expect(sources[0]).toBe('arale.alipay.im:8001');
+      expect(sources[1]).toBe('arale.alipay.im:8000');
+      expect(sources[2]).toBe('http://modules.aralejs.org');
+
+      // test object merge
+      var dependencies = config.get('dependencies');
+      expect(dependencies['widget']).toBe('1.0.0');
+
+      // test extend
+      expect(config.get('extend')).toBe(true);
+
+      // test boolean value merge
+      expect(config.get('isBoolean')).toBeFalsy();
+    });
+  });
+
+  it('test read seajs config', function() {
+    runs(function() {
+      config.addFile(json2);
+      config.addFile(seajs_config);
+    });
+
+    waitsFor(function() {
+      return config.isEnd();
+    }, 100);
+
+    runs(function() {
+      expect(config.get('alias')).toBeUndefined();
+      expect(config.get('dependencies')['_']).toEqual('underscore/1.0.0/underscore');
+    });
+  });
+
+  it('test read parent', function() {
+    config.addParseRule('parent', function(value, filepath) {
+       var parentPath = path.join(path.dirname(filepath), value);
+       this.addFile(parentPath, null, true);
+    });
+
+    runs(function() {
+      config.addFile(json2);
+      config.addFile(json3);
+    }); 
+
+    waitsFor(function() {
+      return config.isEnd();
+    }, 100);
+
+    runs(function() {
+      var dependencies = config.get('dependencies');
+      expect(dependencies['$']).toBe('jquery/1.7.2/jquery');
+      expect(config.get('root')).toBe('alipay');
+      expect(config.get('sources')[3]).toBe('arale.alipay.im:8003');
+    });
   });
 });
