ee6537f1e1183250d79cf839d347ad51f73c19d7
kangpangpang <kangpangpang@gmail.com>
2012-09-18 21:30:28 +0800

    support require css

--- test/data/examples/public/app/0.0.1/app-debug.js
+++ test/data/examples/public/app/0.0.1/app-debug.js
@@ -5,8 +5,6 @@ define("#app/0.0.1/sub_mod-debug", [], function(require, exports) {
 });
 
 
-
-
 define("#xbox/0.0.1/xbox-debug", [], function(require, exports) {
   exports.show = function() {
     alert('hi, Im a xbox!');
@@ -21,7 +19,7 @@ define("#apop/0.0.1/apop-debug", [], function(require, exports) {
 });
 
 
-define("#app/0.0.1/app-debug", ["./sub_mod-debug", "./style.css-debug", "#xbox/0.0.1/xbox-debug", "#apop/0.0.1/apop-debug"], function(require, exports) {
+define("#app/0.0.1/app-debug", ["./sub_mod-debug", "#xbox/0.0.1/xbox-debug", "#apop/0.0.1/apop-debug"], function(require, exports) {
   var xbox = require('#xbox/0.0.1/xbox-debug');
   var apop = require('#apop/0.0.1/apop-debug');
   var sub = require('./sub_mod-debug');
