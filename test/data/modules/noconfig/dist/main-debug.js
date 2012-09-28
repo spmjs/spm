9d18929e0380418c612302b85532722186df0777
kangpangpang <kangpangpang@gmail.com>
2012-09-28 23:12:01 +0800

    tpl quotes replace check fix #325

--- test/data/modules/noconfig/dist/main-debug.js
+++ test/data/modules/noconfig/dist/main-debug.js
@@ -1,18 +1,18 @@
-define("#public/1.0.0/core/js/config-debug", [], {
+define("#public/core/js/config-debug", [], {
   name: 'config',
   version: '1.0'
 });
 
 
-define("#public/1.0.0/core/js/utils-debug", [], function(require, exports) {
+define("#public/core/js/utils-debug", [], function(require, exports) {
     exports.log = function(str) {
         console.info('[log] ' + str);
     };
 });
 
 
-define("#public/1.0.0/contact/model/m-debug", ["../../core/js/config-debug", "../../core/js/utils-debug"], function(require, exports, module) {
-  var tpl = '<div>hello</div>';
+define("#public/contact/model/m-debug", ["../../core/js/config-debug", "../../core/js/utils-debug"], function(require, exports, module) {
+  var tpl = '<div>hello</div><input name="nihai" value=\'{"name": "a"}\'/>';
   var config = require('../../core/js/config-debug'); 
   var utils = require('../../core/js/utils-debug'); 
   
@@ -23,10 +23,10 @@ define("#public/1.0.0/contact/model/m-debug", ["../../core/js/config-debug", "..
 });
 
 
-define("#public/1.0.0/main-debug", ["./core/js/config-debug", "./core/js/utils-debug", "./contact/model/m-debug", "#jquery/1.7.2/jquery-debug", "#base/1.0.0/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug"], function(require, exports, module) {
+define("#public/main-debug", ["./core/js/config-debug", "./core/js/utils-debug", "./contact/model/m-debug", "$-debug", "base-debug"], function(require, exports, module) {
   var m = require('./contact/model/m-debug');
-  var $ = require('#jquery/1.7.2/jquery-debug');
-  var base = require('#base/1.0.0/base-debug');
+  var $ = require('$-debug');
+  var base = require('base-debug');
 
   exports.say = function(id) {
     m.say();
