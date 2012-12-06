6a372a99419fa19f756592aef6a2e09c6e17a0fa
Hsiaoming Yang <lepture@me.com>
2012-12-06 20:32:31 +0800

    add test case

---
+++ test/config.js
@@ -0,0 +1,12 @@
+var should = require('should')
+require('../index').logging.setlevel('error')
+var config = require('../lib/core/config')
+
+describe('config', function() {
+  it('should get section.key', function() {
+    config.config('section.key', 'value')
+    config.config('section.key').should.equal('value')
+    config.remove('section')
+    should.not.exist(config.config('section.key'))
+  })
+})
