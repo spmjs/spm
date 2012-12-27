cebbdbe06ae836d800a92d4aa10032d75c5f4fef
Hsiaoming Yang <lepture@me.com>
2012-12-27 15:33:33 +0800

    more meta data in uri resolve

--- tests/httplib.test.js
+++ tests/httplib.test.js
@@ -3,45 +3,66 @@ var require = require('./testutils');
 var httplib = require('../lib/library/httplib');
 
 describe('httplib.resolve', function() {
+  var meta;
+
   it('has no version', function() {
-    var meta = httplib.resolve('lepture/nico');
+    meta = httplib.resolve('lepture/nico');
     should.not.exist(meta.version);
   });
 
   it('has a version', function() {
-    var meta = httplib.resolve('lepture/nico@0.1.5');
+    meta = httplib.resolve('lepture/nico@0.1.5');
+    should.exist(meta.version);
+
+    meta = httplib.resolve('lepture/nico#0.1.5');
+    should.exist(meta.version);
+
+    meta = httplib.resolve('lepture.nico@0.1.5');
     should.exist(meta.version);
   });
 
   it('should resolve as git', function() {
-    var meta = httplib.resolve('lepture/nico');
+    meta = httplib.resolve('lepture/nico');
     meta.type.should.equal('git');
-    meta.url.should.equal('git@github.com:lepture/nico');
-
-    meta = httplib.resolve('lepture/nico@0.1.5');
-    meta.type.should.equal('git');
-    meta.url.should.equal('git@github.com:lepture/nico');
 
     meta = httplib.resolve('git@github.com:lepture/nico');
     meta.type.should.equal('git');
 
     meta = httplib.resolve('https://github.com/lepture/nico.git');
     meta.type.should.equal('git');
+
+    meta = httplib.resolve('git+https://github.com/lepture/nico');
+    meta.type.should.equal('git');
   });
 
   it('should resolve as http', function() {
-    var meta = httplib.resolve('https://github.com/lepture/nico');
+    meta = httplib.resolve('https://github.com/lepture/nico');
     meta.type.should.equal('http');
   });
 
   it('should resolve as spm', function() {
-    var meta = httplib.resolve('lepture.nico');
-    meta.type.should.equal('spm');
-
-    meta = httplib.resolve('lepture.nico@0.1.5');
+    meta = httplib.resolve('lepture.nico');
     meta.type.should.equal('spm');
 
     meta = httplib.resolve('seajs');
     meta.type.should.equal('spm');
+  });
+
+  it('has root: arale', function() {
+    // git type
+    meta = httplib.resolve('arale');
+    meta.root.should.equal('arale');
+
+    meta = httplib.resolve('arale/base');
+    meta.root.should.equal('arale');
+
+    meta = httplib.resolve('arale.base');
+    meta.root.should.equal('arale');
+
+    meta = httplib.resolve('git@github.com:aralejs/base');
+    meta.root.should.equal('aralejs');
+
+    meta = httplib.resolve('git://github.com/aralejs/base.git');
+    meta.root.should.equal('aralejs');
   });
 });
