b3cd2e259b1843a6822c91a5c593b183cde5c4fd
Hsiaoming Yang <lepture@me.com>
2013-01-05 17:10:59 +0800

    building standard-cases

---
+++ tests/8.standard-cases-build.test.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+var require = require('./testutils');
+var build = require('../lib/builtin/build');
+
+var casedir = path.join(__dirname, 'standard-cases');
+var cases = fs.readdirSync(casedir).filter(function(dir) {
+  return fs.statSync(path.join(casedir, dir)).isDirectory();
+})
+
+describe('building cases', function() {
+  cases.forEach(function(name) {
+    var absdir = path.join(casedir, name);
+    it(name, function() {
+      build.run({
+        config: path.join(absdir, 'package.json'),
+        inputDirectory: path.join(absdir, 'src'),
+        outputDirectory: path.join(absdir, 'dist')
+      });
+    });
+  });
+});
