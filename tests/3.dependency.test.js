7796b44045ef38a766b65b800cde8f30e1a63450
Hsiaoming Yang <lepture@me.com>
2013-01-11 20:57:02 +0800

    dependency with test case now

---
+++ tests/3.dependency.test.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var should = require('should');
+var require = require('./testutils');
+var dependency = require('../lib/sdk/dependency');
+
+var datadir = path.join(__dirname, 'data');
+
+describe('dependency.getDependencies', function() {
+  it('get no dependencies', function() {
+    var file = path.join(datadir, 'no-deps.js');
+    var deps = dependency.getDependencies(file)
+    deps.should.have.length(0);
+  });
+  it('get a relative dependency', function() {
+    var file = path.join(datadir, 'one-relative-dep.js');
+    var deps = dependency.getDependencies(file)
+    deps.should.have.length(1);
+    deps.should.include('./no-deps')
+  });
+  it('get a chain of relative dependencies', function() {
+    var file = path.join(datadir, 'chain-dep4.js');
+    var deps = dependency.getDependencies(file)
+    deps.should.have.length(4);
+  });
+});
