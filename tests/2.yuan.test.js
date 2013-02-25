939c6abd81bbe7e3af9060e5731ab8c4d8f49272
Hsiaoming Yang <lepture@me.com>
2013-02-25 11:20:23 +0800

    fix test case

--- tests/2.yuan.test.js
+++ tests/2.yuan.test.js
@@ -5,8 +5,8 @@ var yuan = _require('../lib/sdk/yuan');
 
 describe('yuan.login', function() {
   it('can log in yuan', function() {
-    var service = new yuan.Yuan({username: 'spm', password: 'spm'});
+    var service = yuan();
     sinon.stub(service, 'request');
-    service.login(sinon.spy());
+    service.login({username: 'spm', password: 'spm'}, sinon.spy());
   });
 });
