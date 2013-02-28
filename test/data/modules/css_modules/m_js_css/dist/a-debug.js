48290b19cbf8316d5e606d97e47da50d8647867c
kangpangpang <kangpangpang@gmail.com>
2013-02-28 10:28:11 +0800

    update testcase

--- test/data/modules/css_modules/m_js_css/dist/a-debug.js
+++ test/data/modules/css_modules/m_js_css/dist/a-debug.js
@@ -1,3 +1,16 @@
-define("alice/m_js_css/1.0.0/a-debug", [ "./base2.css" ], function(require, exports) {
-    var c = require("./base2.css");
+define("alice/m_js_css/1.0.0/a-debug", [], function(require, exports) {
+    var c = require("./a.css");
+});
+
+define("alice/m_js_css/1.0.0/a.css", [], function() {
+    function importStyle(cssText) {
+        var element = document.createElement("style");
+        doc.getElementsByTagName("head")[0].appendChild(element);
+        if (element.styleSheet) {
+            element.styleSheet.cssText = cssText;
+        } else {
+            element.appendChild(doc.createTextNode(cssText));
+        }
+    }
+    importStyle(".a{color:#000;background:#fff}");
 });
--- test/data/modules/css_modules/m_js_css/dist/b-debug.js
+++ test/data/modules/css_modules/m_js_css/dist/b-debug.js
@@ -1,4 +1,30 @@
-define("alice/m_js_css/1.0.0/b-debug", [ "./base2.css", "./base1.css" ], function(require, exports) {
-    var c = require("./base2.css");
+define("alice/m_js_css/1.0.0/b-debug", [], function(require, exports) {
+    var c = require("./a.css");
     var d = require("./base1.css");
+});
+
+define("alice/m_js_css/1.0.0/a.css", [], function() {
+    function importStyle(cssText) {
+        var element = document.createElement("style");
+        doc.getElementsByTagName("head")[0].appendChild(element);
+        if (element.styleSheet) {
+            element.styleSheet.cssText = cssText;
+        } else {
+            element.appendChild(doc.createTextNode(cssText));
+        }
+    }
+    importStyle(".a{color:#000;background:#fff}");
+});
+
+define("alice/m_js_css/1.0.0/base1.css", [], function() {
+    function importStyle(cssText) {
+        var element = document.createElement("style");
+        doc.getElementsByTagName("head")[0].appendChild(element);
+        if (element.styleSheet) {
+            element.styleSheet.cssText = cssText;
+        } else {
+            element.appendChild(doc.createTextNode(cssText));
+        }
+    }
+    importStyle(".base1{color:#000;background:#fff}");
 });
