d2e571e0b75865d8a82d79d3435384bc55bc55be
Hsiaoming Yang <lepture@me.com>
2013-03-08 19:43:04 +0800

    add git remote

---
+++ tests/2.git.test.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var should = require('should');
+var _require = require('./_require');
+var git = _require('../lib/sdk/git');
+
+describe('git', function() {
+  it('get revision', function(done) {
+    git.revision(function(err, stdout) {
+      stdout.trim().length.should.equal(40);
+      done();
+    });
+  });
+
+  it('get remote', function(done) {
+    git.remote(function(err, remote) {
+      remote.should.equal('git@github.com:spmjs/spm2');
+      done();
+    });
+  });
+});
