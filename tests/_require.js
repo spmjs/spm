eb6f2c3071eed0198d888e056d831e77dfd35567
afc163 <afc163@gmail.com>
2014-01-23 16:52:02 +0800

    new test cases

--- tests/_require.js
+++
@@ -1,9 +0,0 @@
-var log = require('..').log;
-log.quiet = true;
-
-module.exports = function(file) {
-  if (process.env.SPM_COVERAGE) {
-    file = file.replace('/lib/', '/lib-cov/');
-  }
-  return require(file);
-};
--- tests/module-cases/one-dist/a.js
+++
@@ -1,3 +0,0 @@
-define('id', ['gallery/jquery/1.7.2/jquery'], function(require) {
-  require('widget')
-})
