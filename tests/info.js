99180d4402237ba5638b186030a2c834e2539bd9
afc163 <afc163@gmail.com>
2014-01-24 21:01:08 +0800

    add test cases for info

---
+++ tests/info.js
@@ -0,0 +1,41 @@
+var should = require('should');
+var info = require('../lib/info');
+var yuan = require('../lib/sdk/yuan');
+var http = require('http');
+var server;
+var port = 17173;
+
+describe('spm info', function() {
+
+  beforeEach(function() {
+    server && server.close();
+  });
+
+  it('module with version', function(done) {
+    server = http.createServer(function(req, res) {
+      req.url.should.be.eql('/repository/arale-base/1.0.0/');
+      req.method.should.be.eql('GET');
+      res.writeHead(200);
+      res.end('[]');
+    }).listen(port, function() {
+      info({
+        query: "arale-base@1.0.0",
+        server: "http://127.0.0.1:" + port
+      });
+      done();
+    });
+  });
+  it('module stable', function(done) {
+    server = http.createServer(function(req, res) {
+      req.url.should.be.eql('/repository/arale-base/');
+      res.writeHead(200);
+      res.end('[{"name": "module-name-for-test"}]');
+    }).listen(port, function() {
+      info({
+        query: "arale-base",
+        server: "http://127.0.0.1:" + port
+      });
+      done();
+    });
+  });
+});
