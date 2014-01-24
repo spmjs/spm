1805fbc3480d03dca30f5e0e60056729cea0b324
afc163 <afc163@gmail.com>
2014-01-24 15:41:01 +0800

    add test case for iduri

---
+++ tests/iduri.js
@@ -0,0 +1,45 @@
+var should = require('should');
+var iduri = require('../lib/sdk/iduri');
+
+describe('iduri', function() {
+  it('name@1.0.0', function() {
+     var data = iduri.resolve('name@1.0.0');
+     data.name.should.eql('name');
+     data.version.should.eql('1.0.0');
+  });
+  it('name@', function() {
+    var data = iduri.resolve('name@');
+    should.not.exist(data);
+  });
+  it('name@xxx', function() {
+    var data = iduri.resolve('name@xxx');
+    should.not.exist(data);
+  });
+  it('name', function() {
+    var data = iduri.resolve('name');
+    data.name.should.eql('name');
+    data.version.should.eql('');
+  });
+  it('prefix.name@1.0.0', function() {
+    var data = iduri.resolve('prefix.name@1.0.0');
+    should.not.exist(data);
+  });
+  it('prefix/name@1.0.0', function() {
+    var data = iduri.resolve('prefix/name@1.0.0');
+    should.not.exist(data);
+  });
+  it('prefix-name@1.0.0', function() {
+    var data = iduri.resolve('prefix-name@1.0.0');
+    data.name.should.eql('prefix-name');
+    data.version.should.eql('1.0.0');
+  });
+  it('prefix_name@1.0.0', function() {
+    var data = iduri.resolve('prefix_name@1.0.0');
+    should.not.exist(data);
+  });
+  it('prefixName@0.0.1', function() {
+    var data = iduri.resolve('prefixName@0.0.1');
+    should.not.exist(data);
+  });
+});
+
