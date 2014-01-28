60376bd4d12644d5f460117e0282c308565ae8ec
afc163 <afc163@gmail.com>
2014-01-28 15:21:02 +0800

    add some tests for file

--- tests/log.js
+++ tests/log.js
@@ -33,9 +33,9 @@ describe('log', function() {
     log.info('info', 'info');
     console_info.callCount.should.eql(1);
     console_info.calledWith('           info: info').should.eql(true);
-    log.warn('warn', 'warn');
+    log.warn('', '');
     console_warn.callCount.should.eql(1);
-    log.error('error', 'error');
+    log.error('', '');
     console_error.callCount.should.eql(1);
   });
 
