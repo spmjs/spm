bbcb3c60f9c71fc06a6f43fe686a2971599ac17d
afc163 <afc163@gmail.com>
2014-02-11 17:13:22 +0800

    test case for publish

---
+++ tests/publish.js
@@ -0,0 +1,38 @@
+var should = require('should');
+var publish = require('../lib/publish');
+var http = require('http');
+var server;
+var port = 17176;
+
+describe('spm publish', function() {
+
+  beforeEach(function() {
+    server && server.close();
+  });
+
+  it('publish success', function(done) {
+    server = http.createServer(function(req, res) {
+      req.headers['content-type'].should.eql('application/json');
+      req.headers['accept-encoding'].should.eql('gzip');
+      req.url.should.be.eql('/repository/arale-base/1.0.0/');
+      req.method.should.be.eql('POST');
+      res.writeHead(200);
+      var data = {
+        "urlpath": "xxx",
+        "status": 200
+      };
+      res.end(JSON.stringify(data));
+    }).listen(port, function() {
+      publish.publish({
+        server: "http://127.0.0.1:" + port
+      }, {
+        name: 'arale-base',
+        version: '1.0.0'
+      }, function() {
+        done();
+      });
+    });
+  });
+
+});
+
