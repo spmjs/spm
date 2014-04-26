aaf0348e836717a814528a3c08735edee9303c08
afc163 <afc163@gmail.com>
2014-04-26 21:38:03 +0800

    add test for spm search

---
+++ tests/search.js
@@ -0,0 +1,41 @@
+var should = require('should');
+var search = require('../lib/search');
+var http = require('http');
+var server;
+var port = 12345;
+
+describe('spm search', function() {
+
+  beforeEach(function() {
+    server && server.close();
+    server = null;
+  });
+
+  afterEach(function() {
+    server && server.close();
+    server = null;
+  });
+
+  it('search query', function(done) {
+    server = http.createServer(function(req, res) {
+      req.url.should.be.eql('/repository/search?q=arale');
+      req.method.should.be.eql('GET');
+      res.writeHead(200);
+      var data = {
+        "data": {
+          total: 0,
+          results: []
+        }
+      };
+      res.end(JSON.stringify(data));
+    }).listen(port, function() {
+      search({
+        server: "http://127.0.0.1:" + port,
+        query: 'arale'
+      }, function() {
+        done();
+      });
+    });
+  });
+
+});
