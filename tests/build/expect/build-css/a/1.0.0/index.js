192b51215aae02d36d0c51efe7f471260d48ccd1
popomore <sakura9515@gmail.com>
2014-06-04 12:44:14 +0800

    upgrade umi@0.3.0

--- tests/build/expect/build-css/a/1.0.0/index.js
+++ tests/build/expect/build-css/a/1.0.0/index.js
@@ -1,1 +1,1 @@
-define("a/1.0.0/index",["a/1.0.0/a.css"],function(a){a("a/1.0.0/a.css")}),define("a/1.0.0/a.css",[],function(){seajs.importStyle("ul{margin:0}li{color:red}body{background:0 0}")});
+define("a/1.0.0/index",["a/1.0.0/a.css.js"],function(s){s("a/1.0.0/a.css.js")}),define("a/1.0.0/a.css.js",[],function(){seajs.importStyle("ul{margin:0}li{color:red}body{background:0 0}")});
