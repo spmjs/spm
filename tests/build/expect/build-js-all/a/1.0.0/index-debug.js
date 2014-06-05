5e4306185d68600253599cef9339faae2df6c77e
chencheng <sorrycc@gmail.com>
2014-06-06 07:56:00 +0800

    fix textcase with code format

--- tests/build/expect/build-js-all/a/1.0.0/index-debug.js
+++ tests/build/expect/build-js-all/a/1.0.0/index-debug.js
@@ -1,28 +1,28 @@
-define("a/1.0.0/index-debug", ["a/1.0.0/relative-debug", "d/0.1.1/index-debug", "d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function (require, exports, module) {
+define("a/1.0.0/index-debug", ["a/1.0.0/relative-debug", "d/0.1.1/index-debug", "d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function(require, exports, module) {
   require("a/1.0.0/relative-debug");
   require("d/0.1.1/index-debug");
 });
-define("a/1.0.0/relative-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function (require, exports, module) {
+define("a/1.0.0/relative-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function(require, exports, module) {
   console.log('relative');
   require("b/1.1.0/src/b-debug");
 });
-define("d/0.1.1/index-debug", [], function (require, exports, module) {
-  exports.d = function () {
+define("d/0.1.1/index-debug", [], function(require, exports, module) {
+  exports.d = function() {
     console.log('0.1.1');
   };
 });
-define("b/1.1.0/src/b-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug.tpl"], function (require, exports, module) {
+define("b/1.1.0/src/b-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug.tpl"], function(require, exports, module) {
   require("c/1.1.1/index-debug");
   require("b/1.1.0/src/b-debug.tpl");
 });
-define("c/1.1.1/index-debug", ["d/0.1.0/index-debug"], function (require, exports, module) {
+define("c/1.1.1/index-debug", ["d/0.1.0/index-debug"], function(require, exports, module) {
   require("d/0.1.0/index-debug");
 });
-define("d/0.1.0/index-debug", [], function (require, exports, module) {
-  exports.d = function () {
+define("d/0.1.0/index-debug", [], function(require, exports, module) {
+  exports.d = function() {
     console.log('0.1.0');
   };
 });
-define("b/1.1.0/src/b-debug.tpl", [], function (require, exports, module) {
+define("b/1.1.0/src/b-debug.tpl", [], function(require, exports, module) {
   module.exports = '<div></div>';
 });
--- tests/build/expect/build-js-standalone/a/1.0.0/index-debug.js
+++ tests/build/expect/build-js-standalone/a/1.0.0/index-debug.js
@@ -1,34 +1,36 @@
-(function () {
+(function() {
   /**
    * Sea.js mini 2.3.0 | seajs.org/LICENSE.md
    */
   var define;
   var require;
-  (function (global, undefined) {
+  (function(global, undefined) {
     /**
      * util-lang.js - The minimal language enhancement
      */
     function isType(type) {
-      return function (obj) {
+      return function(obj) {
         return {}.toString.call(obj) == "[object " + type + "]"
       }
     }
     var isFunction = isType("Function")
-    /**
-     * module.js - The core of module loader
-     */
+      /**
+       * module.js - The core of module loader
+       */
     var cachedMods = {}
+
     function Module() {}
     // Execute a module
-    Module.prototype.exec = function () {
+    Module.prototype.exec = function() {
       var mod = this
-      // When module is executed, DO NOT execute it again. When module
-      // is being executed, just return `module.exports` too, for avoiding
-      // circularly calling
+        // When module is executed, DO NOT execute it again. When module
+        // is being executed, just return `module.exports` too, for avoiding
+        // circularly calling
       if (this.execed) {
         return mod.exports
       }
       this.execed = true;
+
       function require(id) {
         return Module.get(id).exec()
       }
@@ -44,7 +46,7 @@
       return exports
     }
     // Define a module
-    define = function (id, deps, factory) {
+    define = function(id, deps, factory) {
       var meta = {
         id: id,
         deps: deps,
@@ -53,18 +55,18 @@
       Module.save(meta)
     }
     // Save meta data to cachedMods
-    Module.save = function (meta) {
+    Module.save = function(meta) {
       var mod = Module.get(meta.id)
       mod.id = meta.id
       mod.dependencies = meta.deps
       mod.factory = meta.factory
     }
     // Get an existed module or create a new one
-    Module.get = function (id) {
+    Module.get = function(id) {
       return cachedMods[id] || (cachedMods[id] = new Module())
     }
     // Public API
-    require = function (id) {
+    require = function(id) {
       var mod = Module.get(id)
       if (!mod.execed) {
         mod.exec()
@@ -72,32 +74,32 @@
       return mod.exports
     }
   })(this);
-  define("a/1.0.0/index-debug", ["a/1.0.0/relative-debug", "d/0.1.1/index-debug", "d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function (require, exports, module) {
+  define("a/1.0.0/index-debug", ["a/1.0.0/relative-debug", "d/0.1.1/index-debug", "d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function(require, exports, module) {
     require("a/1.0.0/relative-debug");
     require("d/0.1.1/index-debug");
   });
-  define("a/1.0.0/relative-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function (require, exports, module) {
+  define("a/1.0.0/relative-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug"], function(require, exports, module) {
     console.log('relative');
     require("b/1.1.0/src/b-debug");
   });
-  define("d/0.1.1/index-debug", [], function (require, exports, module) {
-    exports.d = function () {
+  define("d/0.1.1/index-debug", [], function(require, exports, module) {
+    exports.d = function() {
       console.log('0.1.1');
     };
   });
-  define("b/1.1.0/src/b-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug.tpl"], function (require, exports, module) {
+  define("b/1.1.0/src/b-debug", ["d/0.1.0/index-debug", "c/1.1.1/index-debug", "b/1.1.0/src/b-debug.tpl"], function(require, exports, module) {
     require("c/1.1.1/index-debug");
     require("b/1.1.0/src/b-debug.tpl");
   });
-  define("c/1.1.1/index-debug", ["d/0.1.0/index-debug"], function (require, exports, module) {
+  define("c/1.1.1/index-debug", ["d/0.1.0/index-debug"], function(require, exports, module) {
     require("d/0.1.0/index-debug");
   });
-  define("d/0.1.0/index-debug", [], function (require, exports, module) {
-    exports.d = function () {
+  define("d/0.1.0/index-debug", [], function(require, exports, module) {
+    exports.d = function() {
       console.log('0.1.0');
     };
   });
-  define("b/1.1.0/src/b-debug.tpl", [], function (require, exports, module) {
+  define("b/1.1.0/src/b-debug.tpl", [], function(require, exports, module) {
     module.exports = '<div></div>';
   });
   require("a/1.0.0/index-debug");
