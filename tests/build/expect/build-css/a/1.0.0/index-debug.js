473da995a2de93a2271e6ce70db19ccc76e2fa97
chencheng <sorrycc@gmail.com>
2014-07-03 16:31:58 +0800

    update testcase, but deps should be removed

--- tests/build/expect/build-css/a/1.0.0/index-debug.js
+++ tests/build/expect/build-css/a/1.0.0/index-debug.js
@@ -1,6 +1,6 @@
 define("a/1.0.0/index-debug", ["a/1.0.0/a-debug.css.js"], function(require, exports, module) {
   require("a/1.0.0/a-debug.css.js");
 });
-define("a/1.0.0/a-debug.css.js", [], function(require, exports, module) {
-  seajs.importStyle('ul{margin:0}li{color:red}body{background:0 0}');
+define("a/1.0.0/a-debug.css.js", ["import-style/1.0.0/index-debug", "a/1.0.0/b-debug.css.js", "b/1.1.0/index-debug.css.js"], function(require, exports, module) {
+  require("import-style/1.0.0/index-debug")('ul{margin:0;}li{color:blue;}li{color:red;}body{background:none;}');
 });
--- tests/build/expect/build-css/a/1.0.0/index.js
+++ tests/build/expect/build-css/a/1.0.0/index.js
@@ -1,1 +1,1 @@
-define("a/1.0.0/index",["a/1.0.0/a.css.js"],function(s){s("a/1.0.0/a.css.js")}),define("a/1.0.0/a.css.js",[],function(){seajs.importStyle("ul{margin:0}li{color:red}body{background:0 0}")});
+define("a/1.0.0/index",["a/1.0.0/a.css.js"],function(s){s("a/1.0.0/a.css.js")}),define("a/1.0.0/a.css.js",["import-style/1.0.0/index","a/1.0.0/b.css.js","b/1.1.0/index.css.js"],function(s){s("import-style/1.0.0/index")("ul{margin:0;}li{color:blue;}li{color:red;}body{background:none;}")});
