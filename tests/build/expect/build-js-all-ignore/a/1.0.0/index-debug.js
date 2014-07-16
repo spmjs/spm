238b2ac32d16381a6be556e4d10ac9b6f7535a56
chencheng <sorrycc@gmail.com>
2014-07-16 18:21:00 +0800

    fix testcase

--- tests/build/expect/build-js-all-ignore/a/1.0.0/index-debug.js
+++ tests/build/expect/build-js-all-ignore/a/1.0.0/index-debug.js
@@ -1,8 +1,8 @@
-define("a/1.0.0/index-debug", ["b", "c", "d", "d/0.1.1/index-debug"], function(require, exports, module) {
+define("a/1.0.0/index-debug", ["b", "c", "d"], function(require, exports, module) {
   require("a/1.0.0/relative-debug");
   require("d/0.1.1/index-debug");
 });
-define("a/1.0.0/relative-debug", ["b"], function(require, exports, module) {
+define("a/1.0.0/relative-debug", ["b", "c", "d"], function(require, exports, module) {
   console.log('relative');
   require('b');
 });
