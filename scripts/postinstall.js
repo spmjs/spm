df0ff960ba627ea884ab049eab5fef51a3ad5fae
afc163 <afc163@gmail.com>
2014-08-14 20:21:46 +0800

    spm command completion

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path = require('path');
 var spmrc = require('spmrc');
+var exeq = require('exeq');
 
 var registry = spmrc.get('source.default.url');
 var auth = spmrc.get('source.default.auth');
@@ -10,4 +13,40 @@ if (!spmrc.get('registry') && registry && registry !== 'http://spmjs.io') {
 }
 if (!spmrc.get('auth') && auth) {
   spmrc.set('auth', auth);
+}
+
+if (process.platform !== 'win32') {
+  console.log('Installing spm completion.');
+  exeq([
+    ['cp', path.join(__dirname, '.spm_completion'), spmrc.get('user.home')].join(' ')
+  ]).on('done', function() {
+    var text = '\n. ~/.spm_completion';
+    var profile = spmrc.get('user.home') + '/.profile';
+    var bashRc = spmrc.get('user.home') + '/.bashrc';
+    var bashProfile = spmrc.get('user.home') + '/.bash_profile';
+    var zshRc = spmrc.get('user.home') + '/.zshrc';
+
+    if (process.env.SHELL === '/bin/zsh') {
+      writeProfile(zshRc, text);
+    } else {
+      if (fs.existsSync(profile)) {
+        writeProfile(profile, text);
+      } else if (fs.existsSync(bashRc)) {
+        writeProfile(bashRc, text);
+      } else if (fs.existsSync(bashProfile)) {
+        writeProfile(bashProfile, text);
+      }
+    }
+  });
+}
+
+function writeProfile (file, text) {
+  if (fs.existsSync(file)) {
+    var result = fs.readFileSync(file).toString();
+    if (!/spm_completion/.test(result)) {
+      fs.writeFileSync(file, result + text);
+    }
+  } else {
+    fs.writeFileSync(file, text);
+  }
 }
