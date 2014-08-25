72ecf8735f571786c39957c05d1432fdd3a0e10a
chencheng <sorrycc@gmail.com>
2014-08-25 17:48:45 +0800

    add testcase for umd

---
+++ tests/build/expect/build-js-umd/a/1.0.0/index-debug.js
@@ -0,0 +1,48 @@
+;
+(function() {
+  var d_010_index_debug, b_110_src_b_debugtpl, b_110_src_extra_debug, d_011_index_debug, c_111_index_debug, b_110_src_b_debug, a_100_relative_debug, a_100_index_debug;
+  d_010_index_debug = function(exports) {
+    exports.d = function() {
+      console.log('0.1.0');
+    };
+    return exports;
+  }({});
+  b_110_src_b_debugtpl = function(exports) {
+    exports = '<div></div>';
+    return exports;
+  }();
+  b_110_src_extra_debug = function() {
+    console.log('b-extra');
+  }();
+  d_011_index_debug = function(exports) {
+    exports.d = function() {
+      console.log('0.1.1');
+    };
+    return exports;
+  }({});
+  c_111_index_debug = function() {
+    d_010_index_debug;
+  }();
+  b_110_src_b_debug = function() {
+    c_111_index_debug;
+    b_110_src_b_debugtpl;
+  }();
+  a_100_relative_debug = function() {
+    console.log('relative');
+    b_110_src_b_debug;
+    b_110_src_extra_debug;
+  }();
+  a_100_index_debug = function() {
+    a_100_relative_debug;
+    d_011_index_debug;
+  }();
+  if (typeof exports == "object") {
+    module.exports = a_100_index_debug;
+  } else if (typeof define == "function" && define.amd) {
+    define([], function() {
+      return a_100_index_debug
+    });
+  } else {
+    this["a"] = a_100_index_debug;
+  }
+}());
