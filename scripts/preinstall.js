7f1fa9b30dfad12394d85390c9434604b3d3eb1c
popomore <sakura9515@gmail.com>
2015-01-15 01:15:16 +0800

    feat(preinstall) Improve phantomjs installing

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,19 +1,22 @@
-var exec = require('child_process').exec;
+var exeq = require('exeq');
+var colorful = require('colorful');
 
-if (!process.env.PHANTOMJS_CDNURL) {
-  process.env.PHANTOMJS_CDNURL = 'http://npm.taobao.org/dist/phantomjs';
-  console.log('PHANTOMJS_CDNURL=%s npm install phantomjs', process.env.PHANTOMJS_CDNURL);
-  var install = exec('npm install phantomjs -f', {
-    env: process.env
-  });
+var isPhantomInstalled = true;
+try {
+  require('phantomjs');
+  console.log(colorful.cyan('[spm:preinstall] Phantomjs is installed in %s'), require.resolve('phantomjs'));
+} catch(e) {
+  isPhantomInstalled = false;
+}
 
-  install.stdout.on('data', function(data) {
-    process.stdout.write(data);
+if (!isPhantomInstalled) {
+  if (!process.env.PHANTOMJS_CDNURL) {
+    process.env.PHANTOMJS_CDNURL = 'http://npm.taobao.org/dist/phantomjs';
+  }
+  var cmd = 'npm install phantomjs -g';
+  console.log(colorful.cyan('[spm:preinstall] Use PHANTOMJS_CDNURL=%s %s'),
+    process.env.PHANTOMJS_CDNURL, cmd);
+  exeq(cmd).then(function() {
+    console.log(colorful.cyan('[spm:preinstall] Phantomjs installed in %s'), require.resolve('phantomjs'));
   });
-
-  install.stderr.on('data', function(data) {
-    process.stdout.write(data);
-  });
-} else {
-  console.log('use PHANTOMJS_CDNURL=%s', process.env.PHANTOMJS_CDNURL);
 }
