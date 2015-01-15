365b9d3e4fed5b9068283fa4b8b7549414470f6b
popomore <sakura9515@gmail.com>
2015-01-16 02:52:47 +0800

    test(test) assert console using test-console

--- test/test.js
+++ test/test.js
@@ -1,84 +1,76 @@
 require('should');
 var join = require('path').join;
 var muk = require('muk');
+var stdout = require('test-console').stdout;
 var spmTest = require('../lib/test');
 
 describe('test', function() {
 
-  var write = process.stdout.write;
-  var buffer = '';
-  beforeEach(function() {
-    process.stdout.write = function(buf) {
-      buffer += buf;
-      write.call(process.stdout, buf);
-    };
-  });
-  afterEach(function() {
-    buffer = '';
-    process.stdout.write = write;
-  });
   afterEach(muk.restore);
 
   it('should show testcase', function(done) {
+    var inspect = stdout.inspect();
     muk(process, 'cwd', function() {
       return join(__dirname, 'fixtures/normal');
     });
     spmTest({}, function(err) {
-      muk.restore();
-
       if (err) {
         return done(err);
       }
       try {
-        buffer.should.match(/should pass/);
-        buffer.should.match(/ % Stmts |% Branches/);
-        buffer.should.match(/ index.js    |       100 /);
-        buffer.should.match(/ relative.js |       100 /);
+        var output = inspect.output.join('');
+        output.should.match(/should pass/);
+        output.should.match(/ % Stmts |% Branches/);
+        output.should.match(/ index.js    |       100 /);
+        output.should.match(/ relative.js |       100 /);
       } catch(e) {
         err = e;
       }
+      inspect.restore();
       done(err);
     });
   });
 
   it('should not show coverage', function(done) {
+    var inspect = stdout.inspect();
     muk(process, 'cwd', function() {
       return join(__dirname, 'fixtures/normal');
     });
     spmTest({nocoverage: true}, function(err) {
-      muk.restore();
-
       if (err) {
         return done(err);
       }
       try {
-        buffer.should.match(/should pass/);
-        buffer.should.not.match(/ % Stmts |% Branches/);
+        var output = inspect.output.join('');
+        output.should.match(/should pass/);
+        output.should.not.match(/ % Stmts |% Branches/);
       } catch(e) {
         err = e;
       }
+      inspect.restore();
       done(err);
     });
   });
 
   it('should show lcov with coveralls', function(done) {
+    var inspect = stdout.inspect();
     muk(process, 'cwd', function() {
       return join(__dirname, 'fixtures/normal');
     });
     spmTest({coveralls: true}, function(err) {
-      muk.restore();
-
       if (err) {
         return done(err);
       }
       try {
-        buffer.should.match(/should pass/);
-        buffer.should.match(/ % Stmts |% Branches/);
-        buffer.should.match(/SF:.*test\/fixtures\/normal\/index.js/);
-        buffer.should.match(/BRDA:3,1,0,1/);
+        var output = inspect.output.join('');
+        output.should.match(/should pass/);
+        output.should.match(/ % Stmts |% Branches/);
+        output.should.match(/SF:.*test\/fixtures\/normal\/index.js/);
+        output.should.match(/BRDA:3,1,0,1/);
       } catch(e) {
         err = e;
       }
+      inspect.restore();
       done(err);
     });
   });
